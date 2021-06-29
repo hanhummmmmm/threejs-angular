1e03b83616648a3c938e56b0a1bbe71ffc205d9d
hanhummmmmm <hbcartojano@up.edu.ph>
2021-06-29 16:03:03 +0800

    creating scene component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,15 @@
 import { Component } from '@angular/core';
 
-import {
-  Camera,
-  Material,
-  Texture,
-} from 'three';
-
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
   title = 'threejs-angular';
+
+
+
 }
