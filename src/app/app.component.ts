26bf0c2b5370f6ffb59e46a54e23fb856884f96e
hanhummmmmm <hbcartojano@up.edu.ph>
2021-06-29 15:51:30 +0800

    barebones

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,14 @@
 import { Component } from '@angular/core';
 
+import {
+  Camera,
+  Material,
+  Texture,
+} from 'three';
+
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
